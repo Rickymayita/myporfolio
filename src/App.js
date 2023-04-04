7a9af2cd57a08e4d2698d3b5389f6fa6b21fc3b9
rickymayita <rickymayita@hotmail.com>
2023-04-04 20:39:13 -0300

    new beginning

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import Header from './component/Header/Header';
-import Navbar from './component/Nav/Nav';
 
 function App() {
   return (
     <>
-    <div className='bg-circle1'></div>
-    <div className='bg-circle2'></div>
-    <Header />
-    <Navbar />
-
+    
     </>
   );
 }
