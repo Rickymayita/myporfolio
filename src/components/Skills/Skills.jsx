38b69cd634042a1ba7b62fe75b2f627a2150cd8b
rickymayita <rickymayita@hotmail.com>
2023-04-10 17:04:13 -0300

    Skills resonsive

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -4,37 +4,33 @@ import Card from './Card';
 import { IoLogoJavascript } from "react-icons/io";
 import { AiOutlineHtml5 } from "react-icons/ai";
 import { ImCss3 } from "react-icons/im";
-import { IoLogoNodejs } from "react-icons/io";
-import { IoLogoReact } from "react-icons/io5";
 import { SiRedux } from "react-icons/si";
-import { TbSql } from "react-icons/tb";
 import { DiPhotoshop } from "react-icons/di";
-import { DiIllustrator } from "react-icons/di";
 
 const Skills = () => {
-  return (
-    <Container>
-        <h4>My Top <span className="blue">Skill</span></h4>
-        <h1>What I Do?</h1>
-        <Cards>
-            <Card 
-            Icon={IoLogoJavascript}
-            title={ 'Font' }
-            disc={'Lorem'}
-            />
-            <Card 
-            Icon={ IoLogoReact }
-            title={'Back'}
-            disc={'Lorem'}
-            />
-            <Card 
-            Icon={DiPhotoshop}
-            title={'Desing'}
-            disc={'Lorem'}
-            />
-        </Cards>
-    </Container>
-  )
+    return (
+        <Container>
+            <h4>My Top <span className="blue">Skill</span></h4>
+            <h1>What I Do?</h1>
+            <Cards>
+                <Card
+                    Icon={IoLogoJavascript}
+                    title={'Font'}
+                    disc={'Lorem'}
+                />
+                <Card
+                    Icon={SiRedux}
+                    title={'Back'}
+                    disc={'Lorem'}
+                />
+                <Card
+                    Icon={DiPhotoshop}
+                    title={'Desing'}
+                    disc={'Lorem'}
+                />
+            </Cards>
+        </Container>
+    )
 }
 
 export default Skills
@@ -44,6 +40,9 @@ const Container = styled.div`
     max-width: 1280px;
     margin: 0 auto;
     padding: 3rem 0;
+    @media(max-width: 840px){
+        width: 90%;
+    }
     
     h1{
         padding-top: 1rem;
@@ -53,4 +52,5 @@ const Cards = styled.div`
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
     margin-top: 4rem;
+    gap: 1rem;
 `
