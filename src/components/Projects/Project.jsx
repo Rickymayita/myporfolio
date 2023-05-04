6300c9b17b8942cabcc06fad7ceaf962af769f3b
Ricardo Ariel Maya <rickymayita@hotmail.com>
2023-05-04 03:21:04 -0300

    prueba https://mrickytech.netlify.app/

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -9,7 +9,7 @@ const Project = (props) => {
             <div className="disc">
                 <h1>Descripcion</h1>
                 <p>{disc}
-                    <a href="#">demo</a>
+                    <a href="https://mrickytech.netlify.app/">demo</a>
                 </p>
             </div>
         </Container>
